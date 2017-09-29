91e42025659a99eea40c16478e5b377cb06064df
huangyunhe <huangyunhe@cienet.com.cn>
2017-09-29 14:58:12 +0800

    Build a simple OWIN pipeline

---
+++ Owin.Demo/Middleware/DebugMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AppFunc = System.Func<
+    System.Collections.Generic.IDictionary<string, object>,
+    System.Threading.Tasks.Task
+>;
+
+namespace Owin.Demo.Middleware
+{
+    public class DebugMiddleware
+    {
+        private AppFunc _next;
+
+        public DebugMiddleware(AppFunc next)
+        {
+            _next = next;
+        }
+    }
+}
---
+++ Owin.Demo/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Diagnostics;
+
+//[assembly: OwinStartup(typeof(Owin.Demo.Startup))]
+
+namespace Owin.Demo
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            app.Use(async (ctx, next) =>
+            {
+                Debug.WriteLine("Incoming request: " + ctx.Request.Path);
+                await next();
+                Debug.WriteLine("Outcoming request: " + ctx.Request.Path);
+            });
+
+            app.Use(async (ctx, next) =>
+            {
+                Debug.WriteLine("the next ...");
+                await ctx.Response.WriteAsync("<html><head></head><body><h1>Hello World</h1></body></html>");
+            });
+        }
+    }
+}
